66c2685b93999988cf3d64a3042c2aac9daaad64
d_liyanage <admliyanage@gmail.com>
2020-04-04 18:36:47 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+searchForm.addEventListener('submit', (e) => {
+    // Get search term
+    const searchTerm = searchInput.value;
+
+    // Get sort
+    const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+    console.log(sortBy);
+
+    e.preventDefault();
+});
