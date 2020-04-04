d38e8b177a03f4d3e0a17d5bc4939d73e674818c
d_liyanage <admliyanage@gmail.com>
2020-04-04 21:09:17 +0530

    added all

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import reddit from './redditapi.js';
 
 const searchForm = document.getElementById('search-form');
@@ -24,8 +26,31 @@ searchForm.addEventListener('submit', (e) => {
     searchInput.value = '';
 
     // Search Reddit
-    reddit.search(searchTerm, searchLimit, sortBy);
-
+    reddit.search(searchTerm, searchLimit, sortBy)
+        .then(results => {
+            let output = '<div class="card-columns">';
+            // Loop through Post
+            results.forEach(post => {
+                // Check for image
+                let image = post.preview ? post.preview.images[0].source.url : 'https://images.app.goo.gl/N3MbgEPwoHaxYoFq9';
+                output += `
+                <div class="card">
+                <img class="card-img-top" src="${image}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">${post.title}</h5>
+                        <p class="card-text">${truncateText(post.selftext,100)}</p>
+                        <a href="${post.url}" target="_blank" class="btn btn-primary">Read More</a>
+                        <hr>
+                        <span class="badge badge-secondary">Subreddit: ${post.subreddit}</span>
+                        <span class="badge badge-dark">Score: ${post.score}</span>
+                    </div>
+                </div>
+                `;
+            });
+            output += '</div>';
+            document.getElementById('results').innerHTML = output;
+             
+        });
 
     e.preventDefault();
 });
@@ -55,6 +80,15 @@ function showMessage(message, className){
 
 }
 
+// truncate Text
+function truncateText(text, limit){
+    const shortened = text.indexOf(' ', limit);
+    if (shortened == -1) {
+        return text;
+    }
+
+    return text.substring(0, shortened);
+}
 
 
 
--- redditapi.js
+++ redditapi.js
@@ -3,6 +3,7 @@ export default {
         fetch(`http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
         .then(res => res.json())
         .then(data => data.data.children.map(data => data.data))
+        // .then(data => console.log(data.data.children))
         .catch(err => console.log(err)
         );
     }
